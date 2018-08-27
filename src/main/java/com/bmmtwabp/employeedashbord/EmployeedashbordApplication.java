da722761faf795ae4de751baf5df0ae1ed39645d
fhz <13718913664@163.com>
2018-08-27 14:29:46 +0800

    第一次提交代码

---
+++ src/main/java/com/bmmtwabp/employeedashbord/EmployeedashbordApplication.java
@@ -0,0 +1,20 @@
+package com.bmmtwabp.employeedashbord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: 项目启动类
+ * @Param:
+ * @return:
+ * @Author: fenghouzhi
+ * @Date: 2018/8/27
+ */
+@SpringBootApplication
+public class EmployeedashbordApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EmployeedashbordApplication.class, args);
+  }
+
+}
